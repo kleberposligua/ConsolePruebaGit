72e1bd2264d275bbc229dadb094882d2f5481184
KLEBER POSLIGUA <kleber.posligua@pucese.edu.ec>
2020-11-05 11:55:12 -0500

    Agregar archivos de proyecto.

---
+++ ConsolePruebaGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePruebaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo C#");
+            Console.WriteLine("Integración de VS con Git");
+            Console.ReadKey();
+        }
+    }
+}
