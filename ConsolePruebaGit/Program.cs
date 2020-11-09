1ef0259ccb2d1291a6a91d3e178f62747c50911d
KLEBER POSLIGUA <kleber.posligua@pucese.edu.ec>
2020-11-09 09:29:49 -0500

    Revert "función suma de 2 números"
    This reverts commit dcd6a16fda52ab2fcfbfbc070d60a0d2ff85ab8d.

--- ConsolePruebaGit/Program.cs
+++ ConsolePruebaGit/Program.cs
@@ -23,19 +23,9 @@ namespace ConsolePruebaGit
 
             //Fin de clase
             Console.WriteLine("Time is over...");
-
-            Console.WriteLine("Probando la función");
-            int suma = funcionSuma(10, 20);
-            Console.WriteLine("Resultado función = {0}", suma);
-
             Console.ReadKey();
-        }
 
-        //función suma
-        public static int funcionSuma(int a, int b)
-        {
-            return (a * 10 + b * 100);
+            
         }
-
     }
 }
